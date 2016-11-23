04f3ceda0443b1f4ccf4abbab3742071bbf6b648
kyoshidajp <claddvd@gmail.com>
2016-11-23 20:53:20 +0900

    Fix for heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,3 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( webpack/app )
-Rails.application.config.assets.precompile += %w( *.css )
+Rails.application.config.assets.precompile += %w( *.js *application.css)
