f18a4d95f2b8fbe20c6ef82cfe6b2a4ca8c51116
kyoshidajp <claddvd@gmail.com>
2016-11-26 18:59:55 +0900

    Fix control 208's point data

--- app/controllers/api/v1/results_controller.rb
+++ app/controllers/api/v1/results_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ResultsController < ApplicationController
   def index
-    results = Result.all
+    results = Result.limit(100)
     render json: results, include: [:players, :controls]
   end
 end
