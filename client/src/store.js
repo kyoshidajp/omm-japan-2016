cdf2553939bd6cc4c50fa1b56ff59160f3365dca
kyoshidajp <claddvd@gmail.com>
2016-12-16 20:46:22 +0900

    Apply redux-devtools

--- client/src/store.js
+++ client/src/store.js
@@ -2,8 +2,10 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import Thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
+import DevTools from './dev-tools';
 
 export default function createFinalStore() {
-  const finalCreateStore = compose(applyMiddleware(Thunk))(createStore);
+  const finalCreateStore = compose(applyMiddleware(Thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore);
   return finalCreateStore(rootReducer);
 }
