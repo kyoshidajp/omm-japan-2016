05226b156106c345d3090402b3a38ea7168970f1
kyoshidajp <claddvd@gmail.com>
2016-12-17 22:54:10 +0900

    Delete unused module import

--- client/src/store.js
+++ client/src/store.js
@@ -2,7 +2,6 @@ import { applyMiddleware, compose, createStore } from 'redux';
 import Thunk from 'redux-thunk';
 
 import rootReducer from './reducers';
-import DevTools from './dev-tools';
 
 export default function createFinalStore() {
   const finalCreateStore = compose(applyMiddleware(Thunk),
