d91461886fe3ad3eb47f6b0142331c28b2e53df9
kyoshidajp <claddvd@gmail.com>
2016-12-18 11:51:02 +0900

    Add plugin definition to use the production build when production env

--- webpack-production.config.js
+++ webpack-production.config.js
@@ -20,7 +20,18 @@ module.exports = {
         }
       },
     ],
-    plugins: []
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
   }
 };
 
