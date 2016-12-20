b34612704815526855f253f9039b98f25e424414
kyoshidajp <claddvd@gmail.com>
2016-12-20 20:23:45 +0900

    Fix eslint error

--- client/test/actions/searchActionTest.js
+++ client/test/actions/searchActionTest.js
@@ -4,21 +4,25 @@ import * as searchActions from '../../src/actions/search';
 describe('actions', () => {
   it('hoverOutResult', () => {
     const actual = searchActions.outResultTableRow();
-    const expected = {type: searchActions.OUT_RESULT_TABLE_ROW}
+    const expected = { type: searchActions.OUT_RESULT_TABLE_ROW };
     assert.deepEqual(actual, expected);
   });
 
   it('hoverResult', () => {
     const actual = searchActions.hoverResultTableRow('bib');
-    const expected = {type: searchActions.HOVER_RESULT_TABLE_ROW,
-                      value: 'bib'}
+    const expected = {
+      type: searchActions.HOVER_RESULT_TABLE_ROW,
+      value: 'bib'
+    };
     assert.deepEqual(actual, expected);
   });
 
-	it('onChangeSearchTarget', () => {
+  it('onChangeSearchTarget', () => {
     const actual = searchActions.onChangeSearchTarget('target');
-    const expected = {type: searchActions.ON_CHANGE_SEARCH_TARGET,
-                      value: 'target'}
+    const expected = {
+      type: searchActions.ON_CHANGE_SEARCH_TARGET,
+      value: 'target'
+    };
     assert.deepEqual(actual, expected);
   });
 });
