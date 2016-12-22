dbe5f2114a6112d252ad5e37d76318ee8bffa47b
kyoshidajp <claddvd@gmail.com>
2016-12-23 08:07:44 +0900

    Delete unused scope

--- app/models/result.rb
+++ app/models/result.rb
@@ -3,8 +3,5 @@ class Result < ApplicationRecord
   has_many :players, -> { distinct }, through: :result_players
   has_many :result_controls, dependent: :destroy
   has_many :controls, -> { order('result_controls.created_at ASC') }, through: :result_controls
-  scope :has_player_name_like, lambda { |name|
-    joins(:players).merge(Player.name_like(name))
-  }
   belongs_to :category
 end
